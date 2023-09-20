59be207c2100149b8d30ebfb202722087cebefe2
Alexej Wolff <awolff@draug.me>
2023-09-20 19:49:25 +0200

    Update auth_key.ps1
    array of auth keys revesting

--- auth_key.ps1
+++ auth_key.ps1
@@ -42,13 +42,15 @@ foreach ($inputString in $content) {
 }
 Write-Host $authKeys.Length "Keys found"
 
+[array]::Reverse($authKeys)
+
 foreach ($authKey in $authKeys) {
     $url = $baseUrl + "?authkey=$authKey&win_mode=fullscreen&authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=301&gacha_id=58776b704143c91eafe5f8f732a84821bd4be7e3&gacha_type=301&page=1&size=1&end_id=0&lang=ru"
     $response = Invoke-RestMethod -Uri $url -Method Get -ContentType 'application/json'
 
     if($response.message -eq "OK") {
         Set-Clipboard -Value $authkey
-        Write-Host "authkey value:`n`n$authkey"
+        Write-Host "authkey value:`n`n$authkey`n`n"
         Write-Host "URL: "$url
         break
     } else {
