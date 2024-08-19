70a15e9167eedb0bd4f818c6b93f53b2e053fadb
Draug <awolff@draug.me>
2024-08-19 22:22:32 +0200

    Update auth_key.ps1
    game version update to 2.27.0.0

--- auth_key.ps1
+++ auth_key.ps1
@@ -20,7 +20,7 @@ if ($matches.Length -eq 0) {
 }
 
 $gamedir = $matches[1]
-$cachefile = "$gamedir/webCaches/2.24.0.0/Cache/Cache_Data/data_2"
+$cachefile = "$gamedir/webCaches/2.27.0.0/Cache/Cache_Data/data_2"
 $tmpfile = "$env:TEMP/ch_data_2"
 Write-Output $cachefile
 Copy-Item $cachefile -Destination $tmpfile
@@ -43,7 +43,7 @@ foreach ($inputString in $content) {
 Write-Host $authKeys.Length "Keys found"
 
 foreach ($authKey in $authKeys) {
-    $url = $baseUrl + "?authkey=$authKey&win_mode=fullscreen&authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=301&gacha_type=301&page=1&size=20&end_id=0&lang=ru"
+    $url = $baseUrl + "?authkey=$authKey&win_mode=fullscreen&authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=301&gacha_type=301&page=1&size=20&end_id=0&lang=en"
     $response = Invoke-RestMethod -Uri $url -Method Get -ContentType 'application/json'
 
     if($response.message -eq "OK") {
