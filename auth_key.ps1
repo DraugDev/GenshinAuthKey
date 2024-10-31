cd3b8fc64d4984c25d24526acf53d17c063cd110
Draug <awolff@draug.me>
2024-10-31 17:21:45 +0100

    Update auth_key.ps1
    new version added

--- auth_key.ps1
+++ auth_key.ps1
@@ -20,7 +20,7 @@ if ($matches.Length -eq 0) {
 }
 
 $gamedir = $matches[1]
-$cachefile = "$gamedir/webCaches/2.28.0.0/Cache/Cache_Data/data_2"
+$cachefile = "$gamedir/webCaches/2.30.0.0/Cache/Cache_Data/data_2"
 $tmpfile = "$env:TEMP/ch_data_2"
 Write-Output $cachefile
 Copy-Item $cachefile -Destination $tmpfile
